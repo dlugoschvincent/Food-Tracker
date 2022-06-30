55fe1f10701b835e0b61d04a3802187727f5ecd7
Vincent Dlugosch <dlugoschvincent@gmail.com>
2022-06-30 13:58:34 +0200

    cleaned config removed useless packages

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,9 @@
-import adapter from '@sveltejs/adapter-netlify';
-// import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-auto';
 
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
   preprocess: preprocess({
     postcss: true
   }),
@@ -16,11 +13,6 @@ const config = {
       allowed: ['PUT', 'PATCH', 'DELETE']
     },
     adapter: adapter()
-    // vite: () => ({
-    //   build: {
-    //     target: ['esnext']
-    //   }
-    // })
   }
 };
 
