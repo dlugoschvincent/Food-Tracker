2e1205535c7334b6da60b40a81a5afa5ad12fe9e
Vincent Dlugosch <dlugoschvincent@gmail.com>
2022-07-05 01:50:41 +0200

    remove: lodash usage

--- src/lib/components/tracker/tracker.svelte
+++ src/lib/components/tracker/tracker.svelte
@@ -2,15 +2,18 @@
   import Datepicker from '$lib/components/tracker/datepicker.svelte';
   import Meal from '$lib/components/tracker/meal.svelte';
   import type { definitions } from 'types/database';
-  import { groupBy } from 'lodash-es';
+  // import { groupBy } from 'lodash-es';
   export let servings: (definitions['UserAteFood'] & { Food: definitions['Food'] })[];
 
-  $: groupedServings = groupBy(servings, 'meal');
+  // $: groupedServings = groupBy(servings, 'meal');
+  $: breakfastServings = servings.filter(({ meal }) => meal === 'Breakfast');
+  $: lunchServings = servings.filter(({ meal }) => meal === 'Lunch');
+  $: dinnerServings = servings.filter(({ meal }) => meal === 'Dinner');
 </script>
 
 <div class="grid gap-4">
   <Datepicker />
-  <Meal servings={groupedServings.Breakfast} meal="Breakfast" />
-  <Meal servings={groupedServings.Lunch} meal="Lunch" />
-  <Meal servings={groupedServings.Dinner} meal="Dinner" />
+  <Meal servings={breakfastServings} meal="Breakfast" />
+  <Meal servings={lunchServings} meal="Lunch" />
+  <Meal servings={dinnerServings} meal="Dinner" />
 </div>
