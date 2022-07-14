c929a8358ed91f66cd796959e9f045a082807f85
Vincent Dlugosch <dlugoschvincent@gmail.com>
2022-07-14 21:23:19 +0200

    fix some colors and theme for pwa

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -2,9 +2,9 @@
   import Logout from './login/logout.svelte';
 </script>
 
-<nav class="bg-orange-300 dark:bg-orange-500 dark:text-light-200">
+<nav class="bg-orange-300 dark:bg-orange-500">
   <div class="container mx-auto grid grid-flow-col p-4 items-center justify-between">
-    <div class="text-xl">
+    <div class="text-xl dark:text-black">
       <a href="/{new Date().toISOString().slice(0, 10)}">Home</a>
     </div>
     <div class="gap-8 hidden items-center justify-end sm:flex">
