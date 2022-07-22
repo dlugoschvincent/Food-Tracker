2037d8218028b4ded6f8d6f664b0477741c41aaf
Vincent Dlugosch <dlugoschvincent@gmail.com>
2022-07-22 15:26:08 +0200

    remove useless file

--- src/lib/stores/userStore.ts
+++
@@ -1,6 +0,0 @@
-// import { writable } from 'svelte/store';
-//
-// import { supabase } from '$lib/supabaseclient';
-//
-// export const user = writable(supabase.auth.user());
-export const penis = 3;
