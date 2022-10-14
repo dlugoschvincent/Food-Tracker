c4f5cd9c5203c26c691466d108df096d80f2f2c5
Vincent Dlugosch <dlugoschvincent@gmail.com>
2022-10-14 10:27:46 +0200

    update deps

--- types/database/database.types.ts
+++ types/database/database.types.ts
@@ -4,85 +4,85 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json }
-  | Json[];
+  | Json[]
 
 export interface Database {
   public: {
     Tables: {
       Food: {
         Row: {
-          bar_code: number;
-          created_at: string | null;
-          name: string;
-          fat: number;
-          carbohydrates: number;
-          protein: number;
-          fibre: number;
-          saturated_fat: number;
-          salt: number;
-          sugar: number;
-        };
+          bar_code: number
+          created_at: string | null
+          name: string
+          fat: number
+          carbohydrates: number
+          protein: number
+          fibre: number
+          saturated_fat: number
+          salt: number
+          sugar: number
+        }
         Insert: {
-          bar_code?: number;
-          created_at?: string | null;
-          name?: string;
-          fat?: number;
-          carbohydrates?: number;
-          protein?: number;
-          fibre?: number;
-          saturated_fat?: number;
-          salt?: number;
-          sugar?: number;
-        };
+          bar_code?: number
+          created_at?: string | null
+          name?: string
+          fat?: number
+          carbohydrates?: number
+          protein?: number
+          fibre?: number
+          saturated_fat?: number
+          salt?: number
+          sugar?: number
+        }
         Update: {
-          bar_code?: number;
-          created_at?: string | null;
-          name?: string;
-          fat?: number;
-          carbohydrates?: number;
-          protein?: number;
-          fibre?: number;
-          saturated_fat?: number;
-          salt?: number;
-          sugar?: number;
-        };
-      };
+          bar_code?: number
+          created_at?: string | null
+          name?: string
+          fat?: number
+          carbohydrates?: number
+          protein?: number
+          fibre?: number
+          saturated_fat?: number
+          salt?: number
+          sugar?: number
+        }
+      }
       UserAteFood: {
         Row: {
-          user_id: string;
-          food_id: number;
-          created_at: string | null;
-          meal_id: number;
-          grams: number;
-          meal: string;
-        };
+          user_id: string
+          food_id: number
+          created_at: string | null
+          meal_id: number
+          grams: number
+          meal: string
+        }
         Insert: {
-          user_id: string;
-          food_id: number;
-          created_at?: string | null;
-          meal_id?: number;
-          grams: number;
-          meal: string;
-        };
+          user_id: string
+          food_id: number
+          created_at?: string | null
+          meal_id?: number
+          grams: number
+          meal: string
+        }
         Update: {
-          user_id?: string;
-          food_id?: number;
-          created_at?: string | null;
-          meal_id?: number;
-          grams?: number;
-          meal?: string;
-        };
-      };
-    };
+          user_id?: string
+          food_id?: number
+          created_at?: string | null
+          meal_id?: number
+          grams?: number
+          meal?: string
+        }
+      }
+    }
     Views: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     Functions: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     Enums: {
-      [_ in never]: never;
-    };
-  };
+      [_ in never]: never
+    }
+  }
 }
 
