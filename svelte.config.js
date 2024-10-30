e0da45519e2aaa0eee2656f7470bc9ea775af3b1
Vincent Dlugosch <dlugoschvincent@gmail.com>
2024-10-30 17:59:29 +0100

    feat: disable csrf protection for now

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,11 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		// Needs solution
+		csrf: {
+			checkOrigin: false,
+		}
 	},
 
 	vitePlugin: {
