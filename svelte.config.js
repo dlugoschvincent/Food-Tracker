208a769d53c86c6b554722ade3d230601cfcc4e5
Vincent Dlugosch <dlugoschvincent@gmail.com>
2024-11-05 23:14:26 +0100

    fix: csrf protection enables and update deps

--- svelte.config.js
+++ svelte.config.js
@@ -4,15 +4,9 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
-
 	kit: {
-		adapter: adapter(),
-		// Needs solution
-		csrf: {
-			checkOrigin: false
-		}
+		adapter: adapter()
 	},
-
 	vitePlugin: {
 		inspector: {
 			showToggleButton: 'always'
